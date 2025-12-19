ff382b2ee8d6202602a4af291ee605193550d05c
bala-amypo <bala@amypo.in>
2025-12-19 08:30:53 +0000

    User commit

---
+++ ServicceImplementations/UserService.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.exception.ValidationException;
+import com.example.demo.repository.UserRepository;
+
+import java.util.List;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    // REQUIRED constructor order
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new ValidationException("Email already exists");
+        }
+        return userRepository.save(user);
+    }
+
+    public User getUser(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
