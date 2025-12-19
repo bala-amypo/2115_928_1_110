d6811004044bf401119171f8207b54814c27e63c
bala-amypo <bala@amypo.in>
2025-12-19 08:54:53 +0000

    User commit

---
+++ Controllers/AuthController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.security.JwtUtil;
+import com.example.demo.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+
+    public AuthController(UserService userService, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public Map<String, String> login(@RequestParam String email) {
+
+        User user = userService.getAllUsers()
+                .stream()
+                .filter(u -> u.getEmail().equals(email))
+                .findFirst()
+                .orElseThrow();
+
+        String token = jwtUtil.generateToken(
+                user.getId(),
+                user.getEmail(),
+                user.getRole()
+        );
+
+        Map<String, String> response = new HashMap<>();
+        response.put("token", token);
+        return response;
+    }
+}
